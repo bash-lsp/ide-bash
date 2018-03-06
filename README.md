1b492620d40c21d98c4d661b0d3e5ca0ecd7b6d2
Mads Hartmann <mads379@gmail.com>
2018-03-06 21:13:54 +0100

    Basic support for Atom

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ide-bash
-Bash language support for Atom-IDE
+# Bash IDE
+
+Bash language server. Uses [Tree Sitter][tree-sitter] and its
+[grammar for Bash][tree-sitter-bash].
+
+## System Requirements
+
+You need to install that language server separately as it depends on native node
+modules.
+
+```bash
+npm i -g bash-language-server
+```
+
+## Features
+
+- [x] Jump to declaration
+- [x] Find references
+- [x] Code Outline & Show Symbols
+- [x] Highlight occurrences
+- [x] Code completion
+- [x] Simple diagnostics reporting
+- [ ] Rename symbol
+
+[tree-sitter]: https://github.com/tree-sitter/tree-sitter
+[tree-sitter-bash]: https://github.com/tree-sitter/tree-sitter-bash
---
+++ src/main.js
@@ -0,0 +1,55 @@
+const ChildProcess = require("child_process");
+const Path = require('path')
+const {AutoLanguageClient} = require('atom-languageclient')
+
+class BashLanguageClient extends AutoLanguageClient {
+
+  getGrammarScopes () {
+    return [ 'source.shell' ]
+  }
+
+  getLanguageName () {
+    return 'Shell Script'
+  }
+
+  getServerName () {
+    return 'BashIDE'
+  }
+
+  async startServerProcess(projectPath) {
+
+    // The bash-language-server uses '#!/usr/bin/env node' to find the version
+    // of node to use so we'll have to wait until the shell environment has been
+    // loaded
+    await new Promise(resolve => atom.whenShellEnvironmentLoaded(resolve));
+
+    const command = 'bash-language-server'
+    const args = ["start"]
+
+    const childProcess = ChildProcess.spawn(command, args, {
+      cwd: projectPath
+    });
+
+    childProcess.on("error", err =>
+      atom.notifications.addError("Unable to start the Bash language server.", {
+        dismissable: true,
+        buttons: [
+          {
+            text: "Install Instructions",
+            onDidClick: () => atom.workspace.open("atom://config/packages/ide-bash")
+          }
+        ],
+        description:
+          "Please make sure you've followed the System Requirements section in the README"
+      })
+    );
+
+    super.captureServerErrors(childProcess, projectPath)
+
+    return childProcess;
+
+  }
+
+}
+
+module.exports = new BashLanguageClient()
